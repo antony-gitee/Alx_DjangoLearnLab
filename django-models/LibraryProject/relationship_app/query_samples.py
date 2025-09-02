b233248b5000faaaf50966b2e836a627cac83ee2
Antony Gitee <antonygitee@gmail.com>
2025-09-03 02:58:06 +0300

    fix: update queries in query_samples.py with correct lookups

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,18 +2,12 @@ from relationship_app.models import Book, Author, Library, Librarian
 
 # List all books in a library
 def list_books_in_library(library_id):
-    books = Book.objects.filter(library_id=library_id)
-    return books
+    return Book.objects.filter(library__id=library_id)
 
 # Query all books by a specific author
 def books_by_author(author_id):
-    books = Book.objects.filter(author_id=author_id)
-    return books
+    return Book.objects.filter(author__id=author_id)
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_id):
-    try:
-        librarian = Librarian.objects.get(library_id=library_id)
-        return librarian
-    except Librarian.DoesNotExist:
-        return None
+    return Librarian.objects.filter(library__id=library_id).first()
