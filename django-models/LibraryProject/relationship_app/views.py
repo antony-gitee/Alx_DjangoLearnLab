a615138b833ca27acce8acc271059d96c595c9ca
antony-gitee <antony.t.gitee@gmail.com>
2025-09-03 04:31:54 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,3 +7,15 @@
 ["from django.contrib.auth import login", "from django.contrib.auth.forms import UserCreationForm"]
 
 ["UserCreationForm()", "relationship_app/register.html"]
+
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+# Helper function to check if user is Admin
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+# Admin-only view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
