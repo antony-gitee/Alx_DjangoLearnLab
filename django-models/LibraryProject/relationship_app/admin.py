5106c38508880e752fc8fb5523153437ce194fcb
antony-gitee <antony.t.gitee@gmail.com>
2025-09-03 04:32:48 +0300

    Update admin.py

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -4,3 +4,15 @@ from .models import Library, Book
 admin.site.register(Library)
 admin.site.register(Book)
 
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+
+# Helper function to check if user is Admin
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+# Admin-only view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
