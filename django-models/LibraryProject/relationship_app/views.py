d1993edf40064b54841fb0350118a7423ba75196
antony-gitee <antony.t.gitee@gmail.com>
2025-09-03 04:09:22 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,5 @@
 ["relationship_app/list_books.html", "Book.objects.all()"]
 
 ["relationship_app/library_detail.html", "library", "from .models import Library"]
+
+["from django.views.generic.detail import DetailView"]
