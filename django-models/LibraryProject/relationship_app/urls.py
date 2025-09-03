693daaa6b1ec62ab9060d52c2f68eb77032395b4
antony-gitee <antony.t.gitee@gmail.com>
2025-09-03 04:41:58 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,3 +7,4 @@ urlpatterns = [
 ]
 ["from .views import list_books"]
 ["views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="]
+ ["add_book/", "edit_book/", "delete_book"]
