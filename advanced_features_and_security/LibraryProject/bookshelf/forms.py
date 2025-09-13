2cf4b76937fe711eb7168b33e608efcf629d986b
antony-gitee <antony.t.gitee@gmail.com>
2025-09-13 04:50:36 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,1 +1,7 @@
+<!-- example: bookshelf/templates/bookshelf/form_example.html -->
+<form method="POST" action="{% url 'create_book' %}">
+    {% csrf_token %}
+    {{ form.as_p }}
+    <button type="submit">Submit</button>
+</form>
 
