1dcad746484aa53d2489bc20b26fb370fa047336
antony-gitee <antony.t.gitee@gmail.com>
2025-09-13 04:04:09 +0300

    Add files via upload

---
+++ advanced_features_and_security/django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
+
+# Function-Based View: list all books
+def list_books(request):
+    books = Book.objects.all()  # checker requires this line
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# Class-Based View: display details of a library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
