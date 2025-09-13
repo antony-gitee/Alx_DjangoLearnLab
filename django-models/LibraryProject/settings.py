3c6ce893cbff1ca08ac57f0b126001be75577cee
antony-gitee <antony.t.gitee@gmail.com>
2025-09-13 03:08:22 +0300

    Update settings.py

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'relationship_app', 
+    'bookshelf',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
