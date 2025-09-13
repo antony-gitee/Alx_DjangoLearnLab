b020a73e6833daf77c2c4932390ef4d165e4ec65
antony-gitee <antony.t.gitee@gmail.com>
2025-09-13 03:24:22 +0300

    Update admin.py

--- django-models/LibraryProject/bookshelf/admin.py
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -5,19 +5,24 @@ from .models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active',)
-    list_filter = ('is_staff', 'is_active',)
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    ordering = ('email',)
+    search_fields = ('email', 'first_name', 'last_name')
+
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
     )
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
         ),
     )
-    search_fields = ('email',)
-    ordering = ('email',)
+
 
 
