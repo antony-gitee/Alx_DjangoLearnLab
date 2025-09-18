eb2b623129a3d473ea9abfd497852b4dd19f4641
Antony Gitee <antonygitee@gmail.com>
2025-09-19 02:04:48 +0300

    Task 0: Setup Django project with DRF and Book model

---
+++ django-models/LibraryProject/bookshelf/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+["book_list", "raise_exception", "books"]
+from django.contrib.auth.decorators import login_required, permission_required
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,21 @@
-from relationship_app.models import Book, Author, Library, Librarian
+from .models import Author, Book, Library, Librarian
 
-# List all books in a library
-def list_books_in_library(library_id):
-    return Book.objects.filter(library__id=library_id)
+# 1. Query all books by a specific author
+def query_books_by_author(author_name):
+    return Book.objects.filter(author__name=author_name)
 
-# Query all books by a specific author
-def books_by_author(author_id):
-    return Book.objects.filter(author__id=author_id)
+# 2. List all books in a library
+def list_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()
+    except Library.DoesNotExist:
+        return None
 
-# Retrieve the librarian for a library
-def get_librarian_for_library(library_id):
-    return Librarian.objects.filter(library__id=library_id).first()
+# 3. Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return Librarian.objects.get(library=library)
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        return None
