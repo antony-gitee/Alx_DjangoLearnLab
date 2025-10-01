0a3075be5edcad55bd3442f6ad441778cb6a107b
Antony Gitee <antonygitee@gmail.com>
2025-10-02 02:15:03 +0300

    Add Post model with User foreign key

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,7 @@
-# blog/models.py
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
