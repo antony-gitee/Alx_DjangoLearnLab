1a793ad5acfd99b6d6557ffe869f5760b878602e
antony-gitee <antony.t.gitee@gmail.com>
2025-10-02 03:10:50 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -23,3 +23,4 @@ urlpatterns = [
 
 ["post/<int:pk>/update/"]
 ["comment/<int:pk>/update/", "post/<int:pk>/comments/new/", "comment/<int:pk>/delete/"]
+ ["tags/<slug:tag_slug>/", "PostByTagListView.as_view()"]
