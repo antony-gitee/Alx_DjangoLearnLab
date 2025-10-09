c8c38cfe7b7984c880e11a083a7ae89ea708ec2d
Antony Gitee <antonygitee@gmail.com>
2025-10-09 18:29:02 +0300

    Add CustomUser model with bio, profile_picture, and followers

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -2,13 +2,17 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
     bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']  # username still required for now
+    profile_picture = models.ImageField(upload_to="profile_pics/", blank=True, null=True)
+    
+    # Followers: users who follow this user
+    followers = models.ManyToManyField(
+        "self",  
+        symmetrical=False,  
+        related_name="following", 
+        blank=True
+    )
 
     def __str__(self):
-        return self.email
+        return self.username
 
